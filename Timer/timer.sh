683d440b0514b5aa8c49f320c9eb0173494039c8
Fyodor <36414426+Unrealf1@users.noreply.github.com>
2019-08-01 18:02:06 +0300

    This timer can beep

---
+++ Timer/timer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+beep_time="3"
+sleep_time="0"
+prev_volume="30"
+max_volume="55"
+min_volume="0"
+
+if [ ! -z "$1" ]
+  then
+    sleep_time="$1"
+fi
+
+sleep $sleep_time
+cur_volume="$min_volume"
+
+while [ 1 ]; do
+    amixer set Master "$cur_volume%" &> /dev/null
+    timeout -s $beep_time $beep_time speaker-test -t sine -f 1000 -l 1 &> /dev/null
+    amixer set Master "$prev_volume%" &> /dev/null
+    cur_volume="$(($cur_volume + 7))"
+    if [ $cur_volume -ge $max_volume ]
+     then
+        cur_volume="$min_volume"
+    fi
+    sleep 10
+done
+
