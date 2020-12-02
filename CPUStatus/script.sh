48ae4d994c7f65d1d9dbf0abee1f343da3854b88
Unrealf1 <fyodor.bukreev@yandex.ru>
2020-12-03 02:44:30 +0300

    changed frequency readings to /proc/cpuinfo

--- CPUStatus/script.sh
+++ CPUStatus/script.sh
@@ -5,7 +5,7 @@ sensors | grep 'Core'
 
 echo "Frequency:"
 core_number=0
-cpufreq-info | grep 'current CPU' 2>&1 | while read line
+cat /proc/cpuinfo | grep 'cpu MHz' 2>&1 | while read line
 do
 echo "Core $core_number: $line"
 core_number=$((core_number + 1))
